b3bd8c66cad50b06748cc3e5fd97bd912d1bf199
MarcinBalejko <mbbm9@o2.pl>
2019-12-28 14:10:04 +0100

    :white_check_mark::package: Imported Axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Villains from './components/Villains';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {
     villains: []
   }
 
-
+  // componentDidMount() {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos?_limit=7')
+  //     .then(res => this.setState({ todos: res.data }))
+  // }
 
 
   render() {
