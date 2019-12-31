bc8e44953653639496549dccda5a079605be79f6
MarcinBalejko <mbbm9@o2.pl>
2019-12-31 22:44:07 +0100

    :wrench: Adjusted power svg

--- src/components/CharacterPowerCard.js
+++ src/components/CharacterPowerCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import power from '../images/power.svg';
 
 function CharacterPowerCard() {
     return (
         <article id="power-card" class="card">
             <p>Uses Venom which grants him superhuman strength and healing process.</p>
             <div id="power-image">
-                {/* <!-- <img src="images/power.svg" /> --> */}
+                <img src={power} alt="" />
             </div>
             <div id="power-name">
                 <p>super power</p>
